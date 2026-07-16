83c46dab135ff233bab47b268d5a85114e89a9c0
Jonatas Luis Ramos Simoes <jonatasimoes.js@gmail.com>
2026-07-16 18:55:39 -0300

    feat: Adicionar funcionalidades de gerenciamento de crianças nas turmas, incluindo visualização e exportação para Excel

--- ebf_project/turmas/tests.py
+++ ebf_project/turmas/tests.py
@@ -1,3 +1,74 @@
-from django.test import TestCase
+from datetime import date
+from io import BytesIO
+from zipfile import ZipFile
+from xml.etree import ElementTree
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from accounts.models import Perfil
+from criancas.models import Crianca
+from .models import Turma
+
+
+class TurmaCriancasExportTests(TestCase):
+    def setUp(self):
+        self.usuario = User.objects.create_user(username='coord', password='senha123')
+        Perfil.objects.create(usuario=self.usuario, tipo_perfil='coordenacao', ativo=True)
+        self.client.login(username='coord', password='senha123')
+
+        self.turma = Turma.objects.create(
+            nome='Turma Azul',
+            faixa_etaria='6 a 8 anos',
+            sala_local='Sala 1',
+            ativa=True,
+        )
+        self.outra_turma = Turma.objects.create(
+            nome='Turma Verde',
+            faixa_etaria='9 a 10 anos',
+            sala_local='Sala 2',
+            ativa=True,
+        )
+        self.crianca = Crianca.objects.create(
+            nome_completo='Ana da Silva',
+            data_nascimento=date(2018, 5, 10),
+            turma=self.turma,
+            ativa=True,
+        )
+        self.outra_crianca = Crianca.objects.create(
+            nome_completo='Bia de Souza',
+            data_nascimento=date(2017, 8, 20),
+            turma=self.outra_turma,
+            ativa=True,
+        )
+
+    def _textos_da_planilha(self, response):
+        with ZipFile(BytesIO(response.content)) as arquivo:
+            xml = arquivo.read('xl/sharedStrings.xml')
+        root = ElementTree.fromstring(xml)
+        namespace = {'xlsx': 'http://schemas.openxmlformats.org/spreadsheetml/2006/main'}
+        return [item.text for item in root.findall('.//xlsx:t', namespace)]
+
+    def test_tela_criancas_da_turma_lista_apenas_criancas_da_turma(self):
+        response = self.client.get(reverse('turmas:criancas', args=[self.turma.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Ana da Silva')
+        self.assertNotContains(response, 'Bia de Souza')
+        self.assertContains(response, reverse('turmas:exportar_criancas_excel', args=[self.turma.id]))
+
+    def test_exportar_criancas_excel_da_turma(self):
+        response = self.client.get(reverse('turmas:exportar_criancas_excel', args=[self.turma.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response['Content-Type'],
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        )
+        self.assertIn('attachment; filename="criancas-turma-azul.xlsx"', response['Content-Disposition'])
+
+        textos = self._textos_da_planilha(response)
+        self.assertIn('Nome completo', textos)
+        self.assertIn('Ana da Silva', textos)
+        self.assertNotIn('Bia de Souza', textos)
--- ebf_project/turmas/views.py
+++ ebf_project/turmas/views.py
@@ -1,8 +1,13 @@
 import uuid
+from io import BytesIO
+from zipfile import ZIP_DEFLATED, ZipFile
+from xml.sax.saxutils import escape
 from django.db import models
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib import messages
+from django.utils.text import slugify
 from django.views.decorators.http import require_POST
 from core.decorators import coordenacao_requerida
 from core.utils import registrar_auditoria
@@ -11,9 +16,170 @@ from .forms import TurmaForm
 from .models import Turma
 
 
+EXCEL_CONTENT_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+
+def _excel_coluna(indice):
+    nome = ''
+    while indice:
+        indice, resto = divmod(indice - 1, 26)
+        nome = chr(65 + resto) + nome
+    return nome
+
+
+def _texto_xml(valor):
+    return escape(str(valor), {'"': '&quot;', "'": '&apos;'})
+
+
+def _gerar_xlsx_simples(linhas):
+    total_linhas = len(linhas)
+    total_colunas = max((len(linha) for linha in linhas), default=1)
+    ultima_celula = f'{_excel_coluna(total_colunas)}{total_linhas or 1}'
+    strings_compartilhadas = []
+    indices_strings = {}
+    total_strings = 0
+
+    def indice_string(valor):
+        nonlocal total_strings
+        texto = str(valor)
+        total_strings += 1
+        if texto not in indices_strings:
+            indices_strings[texto] = len(strings_compartilhadas)
+            strings_compartilhadas.append(texto)
+        return indices_strings[texto]
+
+    linhas_xml = []
+    for linha_idx, linha in enumerate(linhas, start=1):
+        celulas = []
+        for coluna_idx, valor in enumerate(linha, start=1):
+            referencia = f'{_excel_coluna(coluna_idx)}{linha_idx}'
+            estilo = ' s="1"' if linha_idx == 1 else ''
+            if valor in (None, ''):
+                celulas.append(f'<c r="{referencia}"{estilo}/>')
+            elif isinstance(valor, int):
+                celulas.append(f'<c r="{referencia}"{estilo}><v>{valor}</v></c>')
+            else:
+                celulas.append(f'<c r="{referencia}" t="s"{estilo}><v>{indice_string(valor)}</v></c>')
+        linhas_xml.append(f'<row r="{linha_idx}">{"".join(celulas)}</row>')
+
+    worksheet = f'''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+  <dimension ref="A1:{ultima_celula}"/>
+  <sheetViews>
+    <sheetView workbookViewId="0">
+      <pane ySplit="1" topLeftCell="A2" activePane="bottomLeft" state="frozen"/>
+      <selection pane="bottomLeft"/>
+    </sheetView>
+  </sheetViews>
+  <sheetFormatPr defaultRowHeight="15"/>
+  <cols>
+    <col min="1" max="1" width="34" customWidth="1"/>
+    <col min="2" max="2" width="16" customWidth="1"/>
+    <col min="3" max="3" width="10" customWidth="1"/>
+    <col min="4" max="4" width="18" customWidth="1"/>
+    <col min="5" max="5" width="34" customWidth="1"/>
+    <col min="6" max="6" width="18" customWidth="1"/>
+  </cols>
+  <sheetData>
+    {''.join(linhas_xml)}
+  </sheetData>
+  <pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" footer="0.3"/>
+</worksheet>'''
+
+    shared_strings = f'''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="{total_strings}" uniqueCount="{len(strings_compartilhadas)}">
+  {''.join(f'<si><t>{_texto_xml(texto)}</t></si>' for texto in strings_compartilhadas)}
+</sst>'''
+
+    buffer = BytesIO()
+    with ZipFile(buffer, 'w', ZIP_DEFLATED) as arquivo:
+        arquivo.writestr('[Content_Types].xml', '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+  <Default Extension="xml" ContentType="application/xml"/>
+  <Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>
+  <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
+  <Override PartName="/xl/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/>
+  <Override PartName="/xl/sharedStrings.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml"/>
+  <Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/>
+  <Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/>
+</Types>''')
+        arquivo.writestr('_rels/.rels', '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/>
+  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/>
+  <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/>
+</Relationships>''')
+        arquivo.writestr('docProps/core.xml', '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <dc:creator>EBF PIBVP</dc:creator>
+  <cp:lastModifiedBy>EBF PIBVP</cp:lastModifiedBy>
+</cp:coreProperties>''')
+        arquivo.writestr('docProps/app.xml', '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+  <Application>EBF PIBVP</Application>
+</Properties>''')
+        arquivo.writestr('xl/workbook.xml', '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+  <workbookPr/>
+  <bookViews>
+    <workbookView xWindow="0" yWindow="0" windowWidth="16384" windowHeight="8192"/>
+  </bookViews>
+  <sheets>
+    <sheet name="Criancas" sheetId="1" r:id="rId1"/>
+  </sheets>
+  <calcPr calcId="124519" fullCalcOnLoad="1"/>
+</workbook>''')
+        arquivo.writestr('xl/_rels/workbook.xml.rels', '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
+  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/>
+  <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/>
+</Relationships>''')
+        arquivo.writestr('xl/styles.xml', '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+  <fonts count="2"><font><sz val="11"/><color theme="1"/><name val="Calibri"/><family val="2"/></font><font><b/><sz val="11"/><color theme="1"/><name val="Calibri"/><family val="2"/></font></fonts>
+  <fills count="2"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="gray125"/></fill></fills>
+  <borders count="1"><border><left/><right/><top/><bottom/><diagonal/></border></borders>
+  <cellStyleXfs count="1"><xf numFmtId="0" fontId="0" fillId="0" borderId="0"/></cellStyleXfs>
+  <cellXfs count="2"><xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"/><xf numFmtId="0" fontId="1" fillId="0" borderId="0" xfId="0" applyFont="1"/></cellXfs>
+  <cellStyles count="1"><cellStyle name="Normal" xfId="0" builtinId="0"/></cellStyles>
+  <dxfs count="0"/>
+  <tableStyles count="0" defaultTableStyle="TableStyleMedium2" defaultPivotStyle="PivotStyleLight16"/>
+</styleSheet>''')
+        arquivo.writestr('xl/sharedStrings.xml', shared_strings)
+        arquivo.writestr('xl/worksheets/sheet1.xml', worksheet)
+
+    return buffer.getvalue()
+
+
+def _obter_criancas_da_turma(turma):
+    from criancas.models import Crianca, CriancaResponsavel
+
+    vinculos_ativos = CriancaResponsavel.objects.filter(ativo=True).select_related('responsavel').order_by(
+        '-responsavel_principal',
+        'responsavel__nome_completo',
+    )
+    criancas = list(
+        Crianca.objects.filter(turma=turma, ativa=True)
+        .prefetch_related(models.Prefetch('crianca_responsavel', queryset=vinculos_ativos, to_attr='vinculos_ativos'))
+        .order_by('nome_completo')
+    )
+
+    for crianca in criancas:
+        crianca.responsavel_principal_turma = crianca.vinculos_ativos[0].responsavel if crianca.vinculos_ativos else None
+    return criancas
+
+
 @coordenacao_requerida
 def listar_turmas(request):
-    todas = Turma.objects.all().order_by('nome')
+    todas = Turma.objects.annotate(
+        total_criancas_ativas=models.Count(
+            'criancas',
+            filter=models.Q(criancas__ativa=True),
+            distinct=True,
+        )
+    ).order_by('nome')
     status = request.GET.get('status', 'todas')
     busca = request.GET.get('q', '').strip()
 
@@ -40,6 +206,49 @@ def listar_turmas(request):
         'total_ativas': todas.filter(ativa=True).count(),
         'total_inativas': todas.filter(ativa=False).count(),
     })
+
+
+@coordenacao_requerida
+def criancas_da_turma(request, turma_id):
+    turma = get_object_or_404(Turma, id=turma_id)
+    criancas = _obter_criancas_da_turma(turma)
+
+    return render(request, 'turmas/criancas_da_turma.html', {
+        'turma': turma,
+        'criancas': criancas,
+        'total': len(criancas),
+    })
+
+
+@coordenacao_requerida
+def exportar_criancas_excel(request, turma_id):
+    turma = get_object_or_404(Turma, id=turma_id)
+    criancas = _obter_criancas_da_turma(turma)
+
+    linhas = [[
+        'Nome completo',
+        'Código',
+        'Idade',
+        'Data de nascimento',
+        'Responsável principal',
+        'Telefone',
+    ]]
+
+    for crianca in criancas:
+        responsavel = crianca.responsavel_principal_turma
+        linhas.append([
+            crianca.nome_completo,
+            crianca.codigo_interno,
+            crianca.get_idade(),
+            crianca.data_nascimento.strftime('%d/%m/%Y'),
+            responsavel.nome_completo if responsavel else '',
+            responsavel.telefone if responsavel else '',
+        ])
+
+    nome_arquivo = f'criancas-{slugify(turma.nome) or "turma"}.xlsx'
+    response = HttpResponse(_gerar_xlsx_simples(linhas), content_type=EXCEL_CONTENT_TYPE)
+    response['Content-Disposition'] = f'attachment; filename="{nome_arquivo}"'
+    return response
 
 
 @coordenacao_requerida
